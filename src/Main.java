68f15d9f4e4e35e94c4cf1cffd16c5205a708583
Mateo <mateomora002@gmail.com>
2021-05-21 21:05:27 +0200

    la clase main bien del todo

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,9 @@ public class Main {
     public static void main(String[] args) {
         Operaciones operaciones = new Operaciones();
         int[] nums ={};
-       // operaciones.rango(-1, 5);
-        //operaciones.sumRango(5, 1); 
-        //operaciones.isPar(2);
+        operaciones.rango(-1, 5);
+        operaciones.sumRango(5, 1); 
+        operaciones.isPar(2);
         operaciones.extraerPositivos(nums);
         
     }
